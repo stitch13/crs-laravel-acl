d84c86607084201c533979e7d6580da8da5b2221
Nelson M Neto <nelson.marques.neto@gmail.com>
2017-04-07 11:03:20 -0300

    Até aula 10

--- app/Providers/AuthServiceProvider.php
+++ app/Providers/AuthServiceProvider.php
@@ -6,6 +6,7 @@ namespace App\Providers;
 use Illuminate\Foundation\Support\Providers\AuthServiceProvider as ServiceProvider;
 //use App\Post;
 //use App\User;
+use App\Permission;
 
 class AuthServiceProvider extends ServiceProvider
 {
@@ -15,7 +16,7 @@ class AuthServiceProvider extends ServiceProvider
      * @var array
      */
     protected $policies = [
-        \App\Post::class => \App\Policies\PostPolicy::class,
+       // \App\Post::class => \App\Policies\PostPolicy::class,
     ];
 
     /**
@@ -31,5 +32,13 @@ class AuthServiceProvider extends ServiceProvider
         Gate::define('update-post', function(User $user, Post $post){
             return $user->id == $post->user_id;
         });*/
+        
+        $permissions = Permission::with('roles')->get();
+        foreach ($permissions as $permission)
+        {
+            $gate->define($permission->name, function(User $user) use ($permission){
+                return $user->hasPermission($permission);
+            });
+        }
     }
 }
