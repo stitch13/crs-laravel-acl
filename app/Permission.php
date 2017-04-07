aded83d384484071f59365178160285e7d150019
Nelson M Neto <nelson.marques.neto@gmail.com>
2017-04-07 11:26:20 -0300

    Até aula 11

--- app/Permission.php
+++ app/Permission.php
@@ -3,11 +3,30 @@
 namespace App;
 
 use Illuminate\Database\Eloquent\Model;
+use App\Permission;
 
 class Permission extends Model
 {
     public function roles()
     {
-        return $this->belongsToMany(\App\Role::class);
+        $this->belongsToMany(\App\Role::class);
+    }
+    
+    public function hasPermission(Permission $permission)
+    {
+        return $this->hasAnyRoles($permission->roles());
+    }
+    
+    public function hasAnyRoles($roles)
+    {
+        if(is_array($roles) || is_object($roles))
+        {
+            foreach ($roles as $role)
+            {
+                return $this->roles->contains('name', $role->name);
+            }
+        }
+        
+        return $this->roles->contains('name', $roles);
     }
 }
